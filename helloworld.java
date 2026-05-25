1fa83e86aaf19b56745dfea9397db010d0b155df
Samuel Tuvo <samutuvo@yahoo.com>
2026-05-25 11:13:22 +0000

    Stampa del nome 3 volte

--- helloworld.java
+++ helloworld.java
@@ -3,5 +3,7 @@ import java.util.*;
 public class helloworld{
     public static void main(String[] args){
         System.out.println("hello samuel tuvo!");
+        System.out.println("hello samuel tuvo!");
+        System.out.println("hello samuel tuvo!");
     }
 }
