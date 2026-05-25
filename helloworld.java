4894080877db7859226c5105572fb83ea80328fa
Samuel Tuvo <samutuvo@yahoo.com>
2026-05-25 11:14:55 +0000

    Stampa compleanno

--- helloworld.java
+++ helloworld.java
@@ -5,5 +5,6 @@ public class helloworld{
         System.out.println("hello samuel tuvo!");
         System.out.println("hello samuel tuvo!");
         System.out.println("hello samuel tuvo!");
+        System.out.println("04/11/2008");
     }
 }
